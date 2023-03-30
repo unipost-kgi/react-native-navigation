9524cc9fb35ec8b447641cdc24411b8319bfe161
unipost-kgi <kgi1123@unipost.co.kr>
2023-03-30 10:24:36 +0900

    리액트 네이티브 초기셋팅

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
